7b27629998e29efbe7debe08d2d283759a310897
Stephan Vock <stephan.vock@gmail.com>
2015-01-30 23:37:04 +0100

    convert exception implementation

---
+++ examples/convert_exception.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once './../vendor/autoload.php';
+
+class IAmATeaPodException extends \Exception {}
+class UnderConstructionException extends \Exception {}
+
+$app = new \Silex\Application(['debug' => true]);
+
+$app->register(new \Glaubinix\Silex\Provider\ConvertExceptionProvider(),
+    [
+        'qafoo.exception_class_map' => [
+            'IAmATeaPodException' => '419',
+            'UnderConstructionException' => \InvalidArgumentException::class,
+        ],
+    ]
+);
+
+$app->get('/', function() {
+    throw new UnderConstructionException();
+});
+
+// accessing /teapod will return 419 status code
+$app->get('/teapod', function() {
+    throw new IAmATeaPodException();
+});
+
+$app->run();
---
+++ src/Provider/ConvertExceptionProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Glaubinix\Silex\Provider;
+
+use QafooLabs\Bundle\NoFrameworkBundle\EventListener\ConvertExceptionListener;
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class ConvertExceptionProvider implements ServiceProviderInterface
+{
+    public function register(Application $app)
+    {
+        if (!isset($app['qafoo.exception_class_map'])) {
+            $app['qafoo.exception_class_map'] = [];
+        }
+
+        $app['qafoo.listener.convert_exception'] = $app->share(function(Application $app) {
+            return new ConvertExceptionListener($app['logger'], $app['qafoo.exception_class_map']);
+        });
+    }
+
+    public function boot(Application $app)
+    {
+        $app->on(KernelEvents::EXCEPTION, [$app['qafoo.listener.convert_exception'], 'onKernelException']);
+    }
+}
