a6bd22d4169a0d457d67545d82c474ad5d2f937a
Stephan Vock <stephan.vock@gmail.com>
2015-02-08 17:15:00 +0100

    some examples how to use the view service provider

--- examples/view.php
+++ examples/view.php
@@ -1,14 +1,39 @@
 <?php
 
-require_once './../vendor/autoload.php';
+require_once dirname(__DIR__) .'/vendor/autoload.php';
+
+class ExampleController
+{
+    public function example()
+    {
+        return [
+            'name' => 'example',
+        ];
+    }
+}
 
 $app = new \Silex\Application(['debug' => true]);
 
-$app->register(new \Silex\Provider\TwigServiceProvider());
+$app->register(new \Silex\Provider\TwigServiceProvider(),[
+    'twig.path' => [
+        __DIR__ . '/templates',
+    ]
+]);
 $app->register(new \Glaubinix\Silex\Provider\ViewProvider());
 
-$app->get('/', function() {
+$app->get('/closure', function() {
     return [];
 });
+
+// This only works with full qualified namespaces! In this case we dont use a namespace
+$app->get('/namespace', 'ExampleController::example');
+
+// Controller as a service example
+$app->register(new \Silex\Provider\ServiceControllerServiceProvider());
+$app['example.controller'] = $app->share(function() {
+    return new ExampleController();
+});
+
+$app->get('/service', 'example.controller:example');
 
 $app->run();
