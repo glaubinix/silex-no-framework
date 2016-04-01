e553ca910d9854245942e3f3113ba786aa69e782
Stephan Vock <stephan.vock@gmail.com>
2016-04-01 22:30:34 +0200

    Features: web test case for redirect route

---
+++ tests/Web/RedirectRouteTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Glaubinix\Tests\Silex\Web;
+
+use Glaubinix\Silex\Provider\RedirectRouteProvider;
+use QafooLabs\MVC\RedirectRoute;
+use Silex\Provider\UrlGeneratorServiceProvider;
+use Symfony\Component\HttpFoundation\Response;
+
+class RedirectRouteTest extends \Silex\WebTestCase
+{
+    public function createApplication()
+    {
+        $app = new \Silex\Application();
+        $app->register(new RedirectRouteProvider());
+        $app->register(new UrlGeneratorServiceProvider());
+
+        return $app;
+    }
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->app->get('/redirect', function () {
+            return new RedirectRoute('dummy');
+        })->bind('redirectTo');
+
+        $this->app->get('/dummy', function () {
+            return new Response();
+        })->bind('dummy');
+    }
+
+    public function testRedirect()
+    {
+        $client = $this->createClient();
+        $client->request('GET', '/redirect');
+
+        $this->assertTrue($client->getResponse()->isRedirect());
+        $this->assertSame($client->getResponse()->headers->get('location'), '/dummy');
+    }
+}
