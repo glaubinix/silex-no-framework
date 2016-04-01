f74b8fe271783b838f8eb61aa8f298225a01f765
Stephan Vock <stephan.vock@gmail.com>
2016-04-01 22:37:04 +0200

    Features: web test case for convert exception

---
+++ tests/Fixtures/IAmATeaPodException.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Glaubinix\Tests\Silex\Fixtures;
+
+class IAmATeaPodException extends \Exception
+{
+}
---
+++ tests/Web/ConvertExceptionTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Glaubinix\Tests\Silex\Web;
+
+use Glaubinix\Silex\Provider\ConvertExceptionProvider;
+use Glaubinix\Tests\Silex\Fixtures\IAmATeaPodException;
+use Silex\WebTestCase;
+
+class ConvertExceptionTest extends WebTestCase
+{
+    public function createApplication()
+    {
+        $app = new \Silex\Application();
+        $app->register(new ConvertExceptionProvider(), [
+            'qafoo.exception_class_map' => [
+                IAmATeaPodException::class => 419,
+            ],
+        ]);
+
+        return $app;
+    }
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->app->get('/exception', function () {
+            throw new IAmATeaPodException();
+        })->bind('exception');
+    }
+
+    public function testConvert()
+    {
+        $client = $this->createClient();
+        $client->request('GET', '/exception');
+
+        $this->assertSame(419, $client->getResponse()->getStatusCode());
+    }
+}
